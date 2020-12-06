491eb743dbbf923afca150abdc6441d8bec941c5
gke-terraform123 <75487437+gke-terraform123@users.noreply.github.com>
2020-12-06 18:29:22 +0000

    Update outputs.tf

--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -2,5 +2,6 @@ output "host" {
   value     = "${google_container_cluster.gke-cluster.endpoint}"
  }
 
-
-
+output "ingress_ip" {
+  value = formatlist("%s ", kubernetes_ingress.example.load_balancer_ingress[*].ip)
+}
