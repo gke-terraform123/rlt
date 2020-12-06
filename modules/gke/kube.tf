7f8230a401ccf1ff4d1a272ef2ad126bb86b9099
gke-terraform123 <75487437+gke-terraform123@users.noreply.github.com>
2020-12-06 18:01:04 +0000

    Update kube.tf

--- modules/gke/kube.tf
+++ modules/gke/kube.tf
@@ -9,18 +9,18 @@ provider "kubernetes" {
     version                 = ">= 1.10.0"
     load_config_file        = false
 
-    host                    = google_container_cluster.gke-cluster.endpoint
+    host                    = data.google_container_cluster.gke-cluster.endpoint
 
     token                   = data.google_client_config.default.access_token
 
-    client_certificate      = base64decode(google_container_cluster.gke-cluster.master_auth[0].client_certificate)
-    client_key              = base64decode(google_container_cluster.gke-cluster.master_auth[0].client_key)
-    cluster_ca_certificate  = base64decode(google_container_cluster.gke-cluster.master_auth[0].cluster_ca_certificate)
+    client_certificate      = base64decode(data.google_container_cluster.gke-cluster.master_auth[0].client_certificate)
+    client_key              = base64decode(data.google_container_cluster.gke-cluster.master_auth[0].client_key)
+    cluster_ca_certificate  = base64decode(data.google_container_cluster.gke-cluster.master_auth[0].cluster_ca_certificate)
 }
 
 resource "null_resource"  "configure_kubectl" {
   provisioner "local-exec" {
-    command = "gcloud container clusters get-credentials ${google_container_cluster.gke-cluster.name} --region ${var.region-name} --project ${var.project-name}"
+    command = "gcloud container clusters get-credentials ${data.google_container_cluster.gke-cluster.name} --region ${var.region-name} --project ${var.project-name}"
   }
   depends_on = [google_container_cluster.gke-cluster]
 }
