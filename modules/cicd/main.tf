f05ca5c421dcefb1809e7b1d400dd7082bb77036
gke-terraform123 <75487437+gke-terraform123@users.noreply.github.com>
2020-12-06 19:32:03 +0000

    Update main.tf

--- modules/cicd/main.tf
+++ modules/cicd/main.tf
@@ -1,21 +1,28 @@
-provider "google" {
+resource "google_sourcerepo_repository" "repo" {
   project = var.project-name
-  region  = var.region
-}
-
-provider "google-beta" {
-  project = var.project-name
-  region  = var.region
+  name    = var.repo-name
 }
 
 resource "google_cloudbuild_trigger" "trigger" {
   project = var.project-name
+  name    = var.trigger-name
    
   
   trigger_template {
     branch_name = "master"
-    repo_name   = var.repo_name
+    repo_name   = google_sourcerepo_repository.repo.name
   }
 
   filename = "cloudbuild.yaml"   
+}
+
+data "google_project" "project" {}
+
+resource "google_project_iam_member" "cloudbuild_roles" {
+    depends_on  = [google_cloudbuild_trigger.trigger]
+    for_each    = toset(["roles/container.developer","roles/iam.serviceAccountUser"])
+
+    project     = var.project-name
+    role        = each.key
+    member      = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
